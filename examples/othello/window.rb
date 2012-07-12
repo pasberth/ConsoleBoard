95fd62ee78bed765dca250656254ff997d92b273
pasberth <pasberth@gmail.com>
2012-07-12 17:52:50 +0900

    ConsoleWindowの名前空間などをしよう

--- examples/othello/window.rb
+++ examples/othello/window.rb
@@ -13,74 +13,81 @@ module Othello
       @othello = Game.new(@board)
 
       frames.on :main do
-        @input.focus!
+        unfocus! :main
+        focus! :command
       end
 
       @info = create_sub(ConsoleWindow::Window, 80, 1, 0, 9)
       @input = create_sub(ConsoleWindow::Window, 80, 1, 0, 10)
 
-      @input.frames.before :main do
-        @input.text = "Input command: "
-      end
+      frames.group :command do |g|
+        g.before :main do
+          @input.text = "Input command: "
+        end
 
-      @input.frames.on :main do
-        case @input.gets
-        when /^put/i then @input.focus!(:put)
-        when /^help\s+(.*)/i then @input.focus!(:help, $1)
-        when /^pass/i then @input.focus!(:pass)
-        when /^exit/i then @input.unfocus!; self.unfocus!
+        g.on :main do
+          case @input.gets
+          when /^put/i then g.focus!(:put) 
+          when /^pass/i then g.focus!(:pass)
+          when /^help\s+(.*)/i then g.focus!(:help, $1)
+          when /^exit/i then g.unfocus!
+          end
+        end
+
+        g.on :help do |help|
+          case help
+          when /^put/i
+            @info.text = "Put a stone onto the board."
+          when /^pass/i
+            @info.text = "Pass the turn."
+          when /^exit/i
+            @info.text = "Exit the game."
+          else
+            @info.text = "Type 'help <cmd>' (cmd is one of 'put', 'pass', 'exit'.)"
+          end
+          g.unfocus!(:help)
+        end
+
+        g.on :pass do
+          @othello.pass
+          g.unfocus!(:pass)
+        end
+
+        g.group :put do |put|
+
+          put.on :main do |; ss|
+            ss = @othello.turn_player.selections
+
+            if ss.empty?
+              @info.text = "Selection has not been found. You can't select."
+              put.unfocus!
+            else
+              put.focus!(:put!, ss)
+              @board.focus!(:select)
+            end
+          end
+
+          put.on :put! do |ss|
+            x, y = @board.board_cursor.x, @board.board_cursor.y
+
+            if ss.include? [x, y]
+              @info.text = "You put it on (#{x}, #{y})."
+              @othello.put(x, y)
+              put.unfocus!
+              g.focus!(:next_turn)
+            else
+              @info.text = "You can't put it at (#{x}, #{y})."
+              put.unfocus!(:put!)
+            end
+          end
+        end
+
+        g.on :next_turn do
+          @info.text = "Next turn is #{@othello.turn_player.color}"
+          g.unfocus! :next_turn
         end
       end
 
-      @input.frames.on :help do |help|
-        case help
-        when /^put/i
-          @info.text = "Put a stone onto the board."
-        when /^pass/i
-          @info.text = "Pass the turn."
-        when /^exit/i
-          @info.text = "Exit the game."
-        else
-          @info.text = "Type 'help <cmd>' (cmd is one of 'put', 'pass', 'exit'.)"
-        end
-        @input.unfocus!(:help)
-      end
-
-      @input.frames.on :pass do
-        @othello.pass
-        @input.unfocus!(:pass)
-      end
-
-      @input.frames.on :put do |; ss|
-        ss = @othello.turn_player.selections
-        if ss.empty?
-          @info.text = "Selection has not been found. You can't select."
-          @input.unfocus!(:put)
-        else
-          @input.focus!(:put!, ss)
-          @board.focus!(:select)
-        end
-      end
-
-      @input.frames.on :put! do |ss|
-        x, y = @board.board_cursor.x, @board.board_cursor.y
-        if ss.include? [x, y]
-          @info.text = "You put it on (#{x}, #{y})."
-          @othello.put(x, y)
-          @input.unfocus!(:put!)
-          @input.unfocus!(:put)
-          @input.focus!(:next_turn)
-        else
-          @info.text = "You can't put it at (#{x}, #{y})."
-          @input.unfocus!(:put!)
-        end
-      end
-
-      @input.frames.on :next_turn do
-        @info.text = "Next turn is #{@othello.turn_player.color}"
-        @input.unfocus! :next_turn
-      end
-      
       self.components << @othello.board << @input << @info
     end
   end
