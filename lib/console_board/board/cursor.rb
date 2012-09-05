5e1ef94aff26dbbf0a41e0a2ddc70f6e5bb80144
pasberth <pasberth@gmail.com>
2012-09-05 16:48:50 +0900

    bugfix

--- lib/console_board/board/cursor.rb
+++ lib/console_board/board/cursor.rb
@@ -8,15 +8,13 @@ module ConsoleBoard
       def x
         window.table.each_row.
           take(window.board_cursor.x).
-          map { |row| row[window.board_cursor.y].width }.
-          inject(0, :+)
+          inject(0) { |r, row| r + row.width }
       end
 
       def y
         window.table.each_culumn.
           take(window.board_cursor.y).
-          map { |cul| cul[window.board_cursor.x].height }.
-          inject(0, :+)
+          inject(0) { |r, cul| r + cul.height }
       end
     end
   end
